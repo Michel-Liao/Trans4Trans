d46f480303fbd9c3aceee537722c0c5610a02626
Michel Liao <michel.liao22@gmail.com>
2024-06-20 15:21:29 -0400

    Update dataloader

--- segmentron/data/dataloader/transobj.py
+++ segmentron/data/dataloader/transobj.py
@@ -137,9 +137,9 @@ class TransObjSegmentation(SegmentationDataset):
     #         else:
     #             print("Reserving {} for validation".format(scene))
 
-    @staticmethod
-    def image_read(image_path):
-        return cv2.cvtColor(cv2.imread(image_path.as_posix()), cv2.COLOR_BGR2RGB)
+    # @staticmethod
+    # def image_read(image_path):
+    #     return cv2.cvtColor(cv2.imread(image_path.as_posix()), cv2.COLOR_BGR2RGB)
 
     @staticmethod
     def npz_read(npz_path, threshold=0.5):
@@ -171,20 +171,21 @@ class TransObjSegmentation(SegmentationDataset):
         png_path = self.png_paths[index]
 
         # img = torch.from_numpy(self.image_read(png_path))  # [H, W, C]
-        img = self.image_read(png_path)  # [H, W, C]
+        # img = self.image_read(png_path)  # [H, W, C]
+        img = Image.open(png_path).convert("RGB")
 
         # if this is a test image, return the image and its name
 
-        # img = Image.open(self.images[index]).convert("RGB")
         if self.mode == "test":
-            # img = self._img_transform(img)
+            img = self._img_transform(img)
             if self.transform is not None:
                 img = self.transform(img)
             return img, os.path.basename(self.png_paths[index])
 
         # ?: Not sure if this will work
         # mask = Image.open(self.masks[index]).convert("P")
-        mask = self.npz_read(npz_path)  # [H, W]
+        # mask = self.npz_read(npz_path)  # [H, W]
+        mask = Image.fromarray(self.npz_read(npz_path), mode="P")
 
         # synchrosized transform
         if self.mode == "train":
@@ -198,7 +199,8 @@ class TransObjSegmentation(SegmentationDataset):
         # general resize, normalize and to Tensor
         if self.transform is not None:
             img = self.transform(img)
-        return img, mask, os.path.basename(self.images[index])
+        # return img, mask, os.path.basename(self.images[index])
+        return img, mask, os.path.basename(self.png_paths[index])
 
         # if self.augmentation:
         #     im, transparency_arr = self.augmentation(im, transparency_arr)
